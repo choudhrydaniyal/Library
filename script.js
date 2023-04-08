a2cc8ff513c5e3435781c200c634398e647db311
Choudhry Daniyal <100271560+choudhrydaniyal@users.noreply.github.com>
2023-04-08 05:07:23 +0500

    Made a seperate function for displaying table

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 let myLibrary = [];
 
-
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function (){
-        console.log(this.title + ' by ' + this.author + ', ' + this.pages + ' pages' + ', ' + this.read + '.');
-    }
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    console.log(
+      this.title +
+        " by " +
+        this.author +
+        ", " +
+        this.pages +
+        " pages" +
+        ", " +
+        this.read +
+        "."
+    );
+  };
 }
 
+function addBookToLibrary() {
+  let title = window.prompt("Title");
+  let author = window.prompt("Author");
+  let pages = window.prompt("No Of Pages");
+  let read = window.prompt("Read or Not Read Yet");
 
-function addBookToLibrary () {
-    let title = window.prompt('Title');
-    let author = window.prompt('Author');
-    let pages = window.prompt('No Of Pages');
-    let read = window.prompt('Read or Not Read Yet');
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  displayTable();
+}
 
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+function displayTable() {
+  let table = document.getElementById("myTable");
+  table.innerHTML = "";
 
-    let table  = document.getElementById("myTable");
-
+  for (let index = 0; index < myLibrary.length; index++) {
     let row = table.insertRow(-1);
 
     let c1 = row.insertCell(0);
@@ -30,19 +43,9 @@ function addBookToLibrary () {
     let c3 = row.insertCell(2);
     let c4 = row.insertCell(3);
 
-
-
-    for (let index = 0; index < myLibrary.length; index++) {
-        let row = table.insertRow(-1);
-
-        let c1 = row.insertCell(0);
-        let c2 = row.insertCell(1);
-        let c3 = row.insertCell(2);
-        let c4 = row.insertCell(3);
-
-        c1.innerText = myLibrary[index].title;
-        c2.innerText = myLibrary[index].author;
-        c3.innerText = myLibrary[index].pages;
-        c4.innerText = myLibrary[index].read;
-    }
+    c1.innerText = myLibrary[index].title;
+    c2.innerText = myLibrary[index].author;
+    c3.innerText = myLibrary[index].pages;
+    c4.innerText = myLibrary[index].read;
+  }
 }
